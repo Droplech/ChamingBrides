f6ec924e40a45a5c5467f3b0ab73235ed62fdde7
Maxim <53229586+Droplech@users.noreply.github.com>
2021-06-08 19:03:25 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -12,6 +12,26 @@ $(document).ready(function(){
     })
 
 
+    $('.burger_btn').click(function(){
+        if(! $('.burger_btn').hasClass('burger-active') ){
+            $('.burger_btn').addClass('burger-active')
+            $('.burger_menu').slideDown()
+        }else{
+            $('.burger_btn').removeClass('burger-active')
+            $('.burger_menu').slideUp()
+        }
+    })
+
+
+    $('.burger_menu li').click(function(){
+        
+        $('.burger_menu li').removeClass('burg_menu-active')
+        $('.burger_menu li').find('.open_list').slideUp()
+        $(this).addClass('burg_menu-active')
+        $(this).find('.open_list').slideDown()
+        
+    })
+
 
 
 
